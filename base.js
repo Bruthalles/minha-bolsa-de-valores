9cedae6d7ebe464de099394c77dcc223d1c52ac3
Bruthalles <thallesgmkr@gmail.com>
2024-12-07 22:06:08 -0300

    estrutura simples inicial

---
+++ base.js
@@ -0,0 +1,8 @@
+const bitcoin = "611.000,00";
+
+function print(message){
+ const display = console.log(message);
+ return display;   
+};
+print("ola mundo do bitcoin");
+print("valores de hoje: bitcoin = R$"+ bitcoin);
